374f314363cdffc0e4a840011755c4da78430dec
Thang <trinhhominhthang2k5@gmail.com>
2024-04-15 09:26:57 +0700

    Copy many

--- testGit/testGit.cpp
+++ testGit/testGit.cpp
@@ -2,5 +2,5 @@
 using namespace std;
 int main()
 {
-    cout << "Hello git hehehehehe";
+    cout << "Hello git hehehehehe"; cout << "Hello git hehehehehe"; cout << "Hello git hehehehehe"; cout << "Hello git hehehehehe"; cout << "Hello git hehehehehe"; cout << "Hello git hehehehehe"; cout << "Hello git hehehehehe"; cout << "Hello git hehehehehe"; cout << "Hello git hehehehehe"; cout << "Hello git hehehehehe"; cout << "Hello git hehehehehe";
 }
